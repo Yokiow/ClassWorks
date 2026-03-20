804aaa99669ed220a7a523a23bd01d343f683dc7
Yokiow <Arishka.drachenko@mail.ru>
2026-03-20 14:16:13 +0300

    Add files via upload

---
+++ 20.03.26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5.Variant00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Number number = new Task1.Number(5);
+            Task1.Number number1 = new Task1.Number(10);
+
+            number.Sum(number1);
+            Console.WriteLine(number.Real);
+            number.Sub(number1);
+            Console.WriteLine(number.Real);
+            number.Mul(number1);
+            Console.WriteLine(number.Real);
+            number.Div(number1);
+            Console.WriteLine(number.Real);
+            number.Neg();
+            Console.WriteLine(number.Real);
+        }
+    }
+}
---
+++ 20.03.26/Task1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5.Variant00
+{
+    public class Task1
+    {
+        public struct Number : INumber
+        {
+            // Поля
+            private double _real;
+            // Свойства
+            public double Real => _real;
+            public double Abs => Math.Abs(_real);
+            public int Sign
+            {
+                get
+                {
+                    if (_real > 0)
+                        return 1;
+                    if (_real < 0)
+                        return -1;
+                    return 0;
+                }
+            }
+            // Конструктор
+
+            // Number num = new Number(-5, 2, 102)
+            public Number(double real)
+            {
+                _real = real;
+            }
+            // Методы
+            public void Sum (INumber other)
+            {
+                _real += other.Real;
+            }
+            public void Sub(INumber other)
+            {
+                _real -= other.Real;
+            }
+            public void Mul(INumber other)
+            {
+                _real *= other.Real;
+            }
+            public void Div(INumber other)
+            {
+                _real /= other.Real;
+            }
+            public void Neg()
+            {
+                _real = -_real;
+            }
+        }
+    }
+}
